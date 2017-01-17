833e3b90e376ca9bee4efa4b33887a8ec8b6c37a
klimets <igor35hh@gmail.com>
2017-01-17 16:22:39 +0200

    admin interface

--- book/book/polls/admin.py
+++ book/book/polls/admin.py
@@ -4,12 +4,21 @@ from django.contrib import admin
 
 from .models import Question, Choise
 
+#class ChoiseInline(admin.StackedInline):
+class ChoiseInline(admin.TabularInline):
+    model = Choise
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     #fields = ['pub_date', 'question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Data information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiseInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choise)
