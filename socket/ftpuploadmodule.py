44e080f734128c41f5b41115e0639b52759d3e15
Igor <igor35hh@gmail.com>
2017-07-16 21:18:06 +0300

    ftp modules

---
+++ socket/ftpuploadmodule.py
@@ -0,0 +1,34 @@
+import os
+from ftpdownloadmodule import configTransfer, connectFtp, isTextKind
+
+def cleanRemotes(cf, connection):
+    if cf.cleanall:
+        for remotename in connection.nlst():
+            try:
+                print('deleting remote', remotename)
+                connection.delete(remotename)
+            except:
+                print('cannot delete remote', remotename)
+        
+def uploadAll(cf, connection):
+    localfiles = os.listdir(cf.localdir)
+    for localname in localfiles:
+        localpath = os.path.join(cf.localdir, localname)
+        print('uploading', localpath, 'to', localname, 'as', end=' ')
+        if isTextKind(localname):
+            localfile = open(localpath, 'rb')
+            connection.storlines('STOR '+localname, localfile)
+        else:
+            localfile = open(localpath, 'rb')
+            connection.storbinary('STOR '+localname, localfile)
+        localfile.close()
+    connection.quit()
+    print('Done:', len(localfiles), 'files uploaded')
+    
+if __name__ == '__main__':
+    cf = configTransfer(site='example.org.ua', rdir='books', user='igor')
+    conn = connectFtp(cf)
+    cleanRemotes(cf, conn)
+    uploadAll(cf, conn)   
+    
+    
